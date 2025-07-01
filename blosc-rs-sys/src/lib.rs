56b29cdd2bf0890d3f0349a396dd5c0cc44af5e8
Barak Ugav <barakugav@gmail.com>
2025-07-01 16:45:26 +0300

    Test blosc-rs-sys linking with Debug and Release in CI

--- blosc-rs-sys/src/lib.rs
+++ blosc-rs-sys/src/lib.rs
@@ -13,3 +13,26 @@ mod c_bridge {
     include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 }
 pub use c_bridge::*;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn check_linking() {
+        let data: [u8; 8] = [1, 2, 3, 4, 5, 6, 7, 8];
+        let mut dest = [0u8; 100];
+        unsafe {
+            crate::blosc_compress_ctx(
+                0,
+                crate::BLOSC_NOSHUFFLE as i32,
+                1,
+                data.len(),
+                data.as_ptr() as *const core::ffi::c_void,
+                dest.as_mut_ptr() as *mut core::ffi::c_void,
+                dest.len(),
+                c"zstd".as_ptr(),
+                0,
+                1,
+            );
+        }
+    }
+}
